7cfe5b2e6ea332f3b7515aa0a0e361a4db80dd9a
Artem <artemti0@gmail.com>
2026-03-13 17:27:33 +0200

    feat: fix error on pages without pagination or list

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,34 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({ hide_on_load: true });
-
-  // chrome.action.setBadgeText({
-  //   text: 'OFF',
-  // });
 });
 
 chrome.webRequest.onCompleted.addListener(
   (response) => {
     if (response.method === 'POST' && response.statusCode === 200) {
-      chrome.tabs.sendMessage(response.tabId, { action: 'bookmark-fetch-complete' });
+      chrome.tabs
+        .sendMessage(response.tabId, { action: 'bookmark-fetch-complete' })
+        .catch(() => {});
     }
   },
   { urls: ['https://api.rmr.rocks/api/bookmark/getAllList'] },
 );
-
-// function reddenPage() {
-//   document.body.style.backgroundColor = 'red';
-// }
-
-// chrome.action.onClicked.addListener((tab) => {
-//   if (!tab.url.includes('chrome://')) {
-//     chrome.scripting.executeScript({
-//       target: { tabId: tab.id },
-//       func: reddenPage
-//     });
-//   }
-// });
-
-//  await chrome.scripting.insertCSS({
-//   files: ["focus-mode.css"],
-//   target: { tabId: tab.id },
-// });
